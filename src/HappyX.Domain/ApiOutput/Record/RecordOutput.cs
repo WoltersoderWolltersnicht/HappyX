edd3bf01de6a9865ba247ca4ea6206113e6b9528
aWolters <asanchez@xmltravelgate.com>
2022-09-20 19:38:50 +0200

    Return User - Records

---
+++ src/HappyX.Domain/ApiOutput/Record/RecordOutput.cs
@@ -0,0 +1,15 @@
+﻿namespace HappyX.Domain.ApiOutput.Record;
+
+public class RecordOutput
+{
+    public RecordOutput(string slackId, DateTime date, string mood)
+    {
+        SlackId = slackId;
+        Date = date;
+        Mood = mood;
+    }
+    
+    public string SlackId { get; set; }
+    public DateTime Date { get; set; }
+    public string Mood { get; set; }
+}
