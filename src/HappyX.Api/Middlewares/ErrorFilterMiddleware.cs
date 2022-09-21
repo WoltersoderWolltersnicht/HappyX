54a32e6d5e60ae9e6978d4806bea3e6f3ecf345c
aWolters <asanchez@xmltravelgate.com>
2022-09-22 00:11:56 +0200

    Error Filter

---
+++ src/HappyX.Api/Middlewares/ErrorFilterMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HappyX.Api.Middlewares;
+
+public class ErrorFilterMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorFilterMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = 500;
+            context.Response.ContentType = "text/plain"; 
+            await context.Response.WriteAsync("Un Expected Error");
+        }
+    }
+}
+
